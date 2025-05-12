7bb45592f8e5f6aa662f8f8e1ab650ce96f24167
strawberry-tree <drinkingstraw33@gmail.com>
2025-05-12 23:12:35 +0900

    [feat]  /rooms 및 /users/rooms 엔드포인트 분리

--- app.py
+++ app.py
@@ -25,6 +25,36 @@ class CustomJSONProvider(JSONProvider):
 
 app.json = CustomJSONProvider(app)
 
+def get_rooms_set_confirmed(confirmed):
+        pipeline = [
+            # confirmed가 False인 도큐먼트만
+            {
+                "$match": {
+                    "confirmed": confirmed,
+                }
+            },
+            # join_user 콜렉션과 조인, 사용자 정보를 가져옴
+            {
+                "$lookup": {
+                    "from": "join_user",
+                    "localField": "_id",
+                    "foreignField": "room_id",
+                    "as": "users"
+                }
+            },
+            # 하단 필드 선택
+            {
+                "$project": {
+                    "title": 1,
+                    "category": 1,
+                    "max_count": 1,
+                    "user_count": {"$size": "$users"}
+                }
+            }
+        ]
+        
+        return list(db.rooms.aggregate(pipeline))
+
 @app.route("/")
 def home():
     return redirect(url_for('get_rooms'))
@@ -32,36 +62,15 @@ def home():
 # 파티 찾기 (GET) - 유저 빼고 완성
 @app.route("/rooms", methods=["GET"])
 def get_rooms():
-    
-    pipeline = [
-        # confirmed가 False인 도큐먼트만
-        {
-            "$match": {
-                "confirmed": False
-            }
-        },
-        # join_user 콜렉션과 조인, 사용자 정보를 가져옴
-        {
-            "$lookup": {
-                "from": "join_user",
-                "localField": "_id",
-                "foreignField": "room_id",
-                "as": "users"
-            }
-        },
-        # 하단 필드 선택
-        {
-            "$project": {
-                "title": 1,
-                "category": 1,
-                "max_count": 1,
-                "user_count": {"$size": "$users"}
-            }
-        }
-    ]
-    
-    rooms = list(db.rooms.aggregate(pipeline))
-    return render_template("room_list.html", rooms=rooms)
+    not_confirmed = get_rooms_set_confirmed(False)
+    return render_template("show_room_list.html", not_confirmed=not_confirmed)
+
+# 신청한 파티 확인하기 (GET) - 유저 빼고 완성
+@app.route("/users/rooms", methods=["GET"])
+def get_user_rooms():
+    confirmed = get_rooms_set_confirmed(True)
+    not_confirmed = get_rooms_set_confirmed(False)
+    return render_template("joined_room_list.html", confirmed=confirmed, not_confirmed=not_confirmed)
 
 # 파티 만들기 화면 띄우기 (GET)
 @app.route("/rooms/create", methods=["GET"])
@@ -79,7 +88,7 @@ def get_modify(rooms_id):
 @app.route("/rooms/<rooms_id>/modify", methods=["POST"])
 def post_modify(rooms_id):
     title = request.form.get("title")
-    max_count = request.form.get("max_count")
+    max_count = int(request.form.get("max_count"))
     category = request.form.get("category")
     
     db.rooms.update_one(
@@ -91,13 +100,13 @@ def post_modify(rooms_id):
         }}
     )
     
-    return redirect(url_for("get_rooms"))
+    return redirect(url_for("get_user_rooms"))
 
 # 파티 만들기 (POST) - 유저 빼고 완성
 @app.route("/rooms/create", methods=["POST"])
 def post_create():
     title = request.form.get("title")
-    max_count = request.form.get("max_count")
+    max_count = int(request.form.get("max_count"))
     category = request.form.get("category")
     
     # rooms 콜렉션에 도큐먼트 추가
@@ -117,14 +126,21 @@ def post_create():
         'room_id': ObjectId(new_room_id)
     }
     db.join_user.insert_one(new_join)
-    return redirect(url_for('get_rooms'))
+    return redirect(url_for('get_user_rooms'))
 
 # 파티 삭제하기 (POST)
 @app.route("/rooms/<rooms_id>/delete", methods=["POST"])
 def post_delete(rooms_id):
     db.rooms.delete_one({"_id": ObjectId(rooms_id)})
     db.join_user.delete_many({"_id": ObjectId(rooms_id)})
-    return redirect(url_for('get_rooms'))
+    return redirect(url_for('get_user_rooms'))
+
+# 파티 확정하기
+@app.route("/rooms/<rooms_id>/confirm", methods=["POST"])
+def post_confirm(rooms_id):
+    db.rooms.update_one({"_id": ObjectId(rooms_id)}, 
+                        {"$set": {'confirmed': True}})
+    return redirect(url_for('get_user_rooms'))
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
