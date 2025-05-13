9ac9a2a353ab873ec8ead7f701414342cd9a2cd5
Sanghwa Bae <baesanghwa1214@gmail.com>
2025-05-13 21:39:22 +0900

    [fix] 서버 연동
    서버 연동

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost',27017)
 db = client.dbroom
 users_collection = db.users
 bcrypt = Bcrypt(app)
@@ -237,4 +238,4 @@ def login():
         return jsonify({'message': '비밀번호가 잘못되었습니다.'}), 400
 
 if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
