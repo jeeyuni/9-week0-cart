a9a31d5b41e7d1884f0f3e4589ce7b6550ba214a
strawberry-tree <drinkingstraw33@gmail.com>
2025-05-14 11:00:30 +0900

    [fix] 현재시각 이후만 설정가능

---
+++ static/formTime.js
@@ -0,0 +1,21 @@
+function getTimeProp(plus) {
+  const now = new Date();
+  if (plus !== 0) {
+    now.setDate(now.getDate() + plus);
+  }
+  now.setSeconds(0, 0); // 초, 밀리초 제거
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
+
+document.getElementById("time").min = getTimeProp(0);
+if (document.getElementById("fixtime")) {
+  document.getElementById("fixtime").min = getTimeProp(0);
+}
+
+document.getElementById("time").value = getTimeProp(1);
